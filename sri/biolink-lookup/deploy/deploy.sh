3583d3ac4752b9d82c5f261b87e7a45824cb8f7c
mapleknight <mapleknight@users.noreply.github.com>
2021-06-14 12:25:19 -0400

    Update deploy.sh

--- sri/biolink-lookup/deploy/deploy.sh
+++ sri/biolink-lookup/deploy/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo ${BUILD_VERSION}
+sed -i.bak \
+    -e "s/DOCKER_IMAGE_VERSION/${BUILD_VERSION}/g" \
+    values.yaml
+rm values.yaml.bak
+
 mv ingress.yaml templates/ingress.yaml
 
 kubectl apply -f namespace.yaml
